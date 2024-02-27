53c7b1f1477bf9511d26350d439c3e313b21808d
mi <mgxxx32123@gmail.com>
2024-02-27 14:50:13 +0900

    ページング出来るように

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
+    
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
